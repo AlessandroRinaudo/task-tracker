e09a8835e0760cfe2f3cdae28682f9eca7824b63
Alessandro <alessandro.rinaudo@gmail.com>
2021-02-28 17:09:04 +0100

    added task generator system

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 const App = () => {
   const [tasks, setTasks] = useState([
     {
-      id:1,
+      id: 1,
       text: 'Doctor Appointment',
       day: 'Feb 5th at 2:30pm',
       remainder: true
     },
     {
-      id:2,
+      id: 2,
       text: 'Meeting at School',
       day: 'Feb 6th at 1:30pm',
       remainder: true
     },
     {
-      id:3,
+      id: 3,
       text: 'Food Shopping',
       day: 'Feb 5th at 2:30pm',
       remainder: false
     },
-  ] )
+  ])
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   //Delete Task 
   const deleteTask = (id) => {
-   setTasks(tasks.filter((task)=> task.id !== id))
+    setTasks(tasks.filter((task) => task.id !== id))
   }
+
+  //Toggle reminder 
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
 
   return (
     <div className="container">
-      <Header title='Task Tracker'/>
-      {tasks.length >0 ?<Tasks tasks = {tasks} onDelete={deleteTask}/>: 'No task to show'}
+      <Header title='Task Tracker' />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No task to show'}
     </div>
   );
 }
